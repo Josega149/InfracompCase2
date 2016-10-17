4e35aed3586e73939cdc1b9997711311b456af71
Jose Gabriel Tamura L <jg.tamura10@uniandes.edu.co>
2016-10-17 15:38:18 -0500

    esqueleto del proyecto

---
+++ Cliente/src/cliente/CifradorAsimetricoRSA.java
@@ -0,0 +1,11 @@
+package cliente;
+
+public class CifradorAsimetricoRSA 
+{
+	/**
+	 * Debe descifrar con la llave publica del server la llave simetrica que se va a usar
+	 * Debe cifrar con la llave privada del cliente la llave simetrica que se va a usar
+	 * Debe cifrar el digest
+	 */
+
+}
