cf144ebe1a4e97ef53d3857feb8c417179e60798
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2016-10-22 15:10:50 -0500

    Holi, Hmac listo

--- Cliente/src/cliente/CifradorHmacMD5.java
+++ Cliente/src/cliente/CifradorHmacMD5.java
@@ -1,7 +1,5 @@
 package cliente;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.security.MessageDigest;
 
 public class CifradorHmacMD5 {
@@ -16,22 +14,37 @@ public class CifradorHmacMD5 {
 		}
 	}
 
-	public byte[] calcular() {
+	public byte[] calcular(String dato) {
 		try {
-			BufferedReader stdIn = new BufferedReader(new	InputStreamReader(System.in));
-			String dato = stdIn.readLine();
 			byte[] text = dato.getBytes();
 			String s1 = new String(text);
-			System.out.println("dato original: " + s1);
+			System.out.println("Dato original: " + s1);
 			byte [] digest = getKeyedDigest(text);
 			String s2 = new String(digest);
-			System.out.println("digest: "+ s2);
+			System.out.println("Digest: "+ s2);
 			return digest;
 		}
 		catch (Exception e) {
-			System.out.println("Excepcion: " + e.getMessage());
+			System.out.println("Excepción: " + e.getMessage());
 			return null;
 		}
+	}
+	
+	public boolean sonIguales(String msj, byte[] digest) {
+		boolean son= true;
+		byte[] mensaje = calcular(msj);
+		if (digest.length == mensaje.length){
+			for (int i=0; i<mensaje.length && son;i++){
+				if (digest[i]!=mensaje[i]) {
+					son = false;
+				}
+			}
+		}
+		else {
+			son = false;
+		}
+		
+		return son;
 	}
 
 }
