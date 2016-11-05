6f38c6e309335751df0b0f0f79507b5d689e1cc1
Jose Gabriel Tamura L <jg.tamura10@uniandes.edu.co>
2016-11-05 14:34:33 -0500

    generator ajustado

--- ClienteSinSeguridad/src/clienteSinSeguridad/Generator.java
+++ ClienteSinSeguridad/src/clienteSinSeguridad/Generator.java
@@ -1,13 +1,25 @@
 package clienteSinSeguridad;
 
+import uniandes.gload.core.*;
+
 public class Generator 
 {
+	private LoadGenerator generator;
+	
+	
 	public Generator()
 	{
-		
+		Task work = createTask();
+		int numberOfTasks = 100;
+		int gapBetweenTasks = 1000;
+		generator = new LoadGenerator("Client - Server Load Test", numberOfTasks, work,gapBetweenTasks );
+		generator.generate();
 	}
 	
-	
+	private Task createTask()
+	{
+		return ClientServerTask();
+	}
 	
 	public static void main(String[] args)
 	{
