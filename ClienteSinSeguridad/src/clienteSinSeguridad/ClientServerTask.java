23632d90f1209d5a6efdf88ec1c5bef67531a90b
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2016-11-05 14:55:24 -0500

    ClientServerTask

---
+++ ClienteSinSeguridad/src/clienteSinSeguridad/ClientServerTask.java
@@ -0,0 +1,32 @@
+package clienteSinSeguridad;
+
+import uniandes.gload.core.Task;
+
+public class ClientServerTask extends Task{
+
+	@Override
+	public void fail() {
+		// TODO Auto-generated method stub
+		System.out.println(Task.MENSAJE_FAIL);
+	}
+
+	@Override
+	public void success() {
+		// TODO Auto-generated method stub
+		System.out.println(Task.OK_MESSAGE);
+	}
+
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		MainClienteSinSeguridad cliente = new MainClienteSinSeguridad();
+		try {
+			cliente.iniciarConversacion();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	
+}
