5642dd7b3c9c201efa4e2751b39d003762bc820d
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2016-11-06 13:23:00 -0500

    mas

--- ClienteSinSeguridad/src/clienteSinSeguridad/MainClienteSinSeguridad.java
+++ ClienteSinSeguridad/src/clienteSinSeguridad/MainClienteSinSeguridad.java
@@ -3,11 +3,9 @@ package clienteSinSeguridad;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.concurrent.TimeUnit;
 
 
@@ -91,18 +89,13 @@ public class MainClienteSinSeguridad
 		try{
 			File tiempos = new File("./data/tiempos");
 			PrintWriter writer = new PrintWriter(new FileWriter(tiempos,true));
-			writer.println("tAutenticación:"+ TimeUnit.MILLISECONDS.toMillis(tiempoAutenticacion));
-			writer.println("tRespuesta:"+ TimeUnit.MILLISECONDS.toMillis(tiempoRespuesta));
+			writer.println("tAutenticación:"+ TimeUnit.MILLISECONDS.convert(tiempoAutenticacion, TimeUnit.NANOSECONDS));
+			writer.println("tRespuesta:"+ TimeUnit.MILLISECONDS.convert(tiempoRespuesta, TimeUnit.NANOSECONDS));
 			writer.close();
 			}catch (Exception e){
 				//:)
 			}
 
 	}
-	/**
-	public static void main(String[] args)
-	{
-		MainClienteSinSeguridad main = new MainClienteSinSeguridad();
-	}*/
 
 }
