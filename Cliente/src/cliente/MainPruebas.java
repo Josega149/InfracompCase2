0fb4af9e391512d0c0f508482b104b49908402a2
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2016-11-08 14:55:41 -0500

    80 y 100

--- Cliente/src/cliente/MainPruebas.java
+++ Cliente/src/cliente/MainPruebas.java
@@ -2,9 +2,9 @@ package cliente;
 
 public class MainPruebas {
 	
-	public final static int NUMBER_OF_TASKS=400; //400, 200  y 80
+	public final static int NUMBER_OF_TASKS=80; //400, 200  y 80
 	
-	public final static int GAP_BETWEEN_TASKS=20; //20, 40 y 100
+	public final static int GAP_BETWEEN_TASKS=100; //20, 40 y 100
 
 	public static void main(String[] args)
 	{
