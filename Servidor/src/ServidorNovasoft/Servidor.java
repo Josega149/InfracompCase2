3b0e459e93a97886b77394237917d39f34220a4a
Margarita Gómez <mm.gomez10@uniandes.edu.co>
2016-11-08 11:10:23 -0500

    400-20-8

--- Servidor/src/ServidorNovasoft/Servidor.java
+++ Servidor/src/ServidorNovasoft/Servidor.java
@@ -32,7 +32,7 @@ public class Servidor   {
 	/**
 	 * Constante que especifica el numero de threads que se usan en el pool de conexiones.
 	 */
-	public static final int N_THREADS = 8;
+	public static final int N_THREADS = 1;
 
 	/**
 	 * Puerto en el cual escucha el servidor. 
